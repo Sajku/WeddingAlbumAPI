2c9dde4447f979718296c0188ebbddcbc06c1845
Aleksandra1500 <Alex_schoepe@wp.pl>
2022-12-10 17:51:51 +0100

    Add comment table  and add and get endpoints.

---
+++ WeddingAlbum.ApplicationServices/Boundaries/ICommentQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeddingAlbum.PublishedLanguage.Dtos;
+using WeddingAlbum.PublishedLanguage.Queries;
+
+namespace WeddingAlbum.ApplicationServices.Boundaries
+{
+    public interface ICommentQuery
+    {
+        Task<List<CommentDTO>> GetComments(GetCommentsParameter query);
+    }
+}
---
+++ WeddingAlbum.Infrastructure/Domain/CommentRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeddingAlbum.ApplicationServices.Boundaries;
+using WeddingAlbum.Domain.Comments;
+using WeddingAlbum.Infrastructure.DataModel.Context;
+
+namespace WeddingAlbum.Infrastructure.Domain
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly WeddingAlbumContext _context;
+
+        public CommentRepository(WeddingAlbumContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(Comment comment)
+        {
+            await _context.Comments.AddAsync(comment);
+        }
+    }
+}
